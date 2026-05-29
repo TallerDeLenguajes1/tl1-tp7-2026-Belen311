4183255a3a71e5bf4f68bbaaee05c5b4eab06389
Beluluuu <sofibelengerman@gmail.com>
2026-05-29 17:27:15 -0300

    Tp terminado

--- SisPersonal/InfoEmpleados.cs
+++ SisPersonal/InfoEmpleados.cs
@@ -1,31 +1,91 @@
 namespace InfoEmpleados;
 
-public enum {"auxiliar", "administrativo", "ingeniero", "especialista", "investigador"}
+public enum Cargos {auxiliar, administrativo, ingeniero, especialista, investigador};
 
 public class Empleado{
     private string nombre;
     private string apellido;
-    private datetime fechaN;
+    private DateTime fechaN;
     private char estadoCivil;
-    private datetime fechaI;
+    private DateTime fechaI;
     private double sueldoBase;
-    private cargos cargo;
+    private Cargos cargo;
 
-    public int antiguo(){
-        return datetime.Now.Year - fechaI.Year;
+    public int Antiguo(){
+        return DateTime.Now.Year - FechaI.Year;
     }
 
     public int Edad{
-        get {return datetime.Now.Year - fechaN} 
+        get {return DateTime.Now.Year - FechaN.Year;}
     }
 
-    public int jubi(){
+    public string Nombre { 
+        get => nombre;
+        }
+    public string Apellido { 
+        get => apellido;
+        }
+    public DateTime FechaN { 
+        get => fechaN;
+        }
+    public char EstadoCivil { 
+        get => estadoCivil;
+        }
+    public DateTime FechaI { 
+        get => fechaI;
+        }
+    public double SueldoBase { 
+        get => sueldoBase;
+        }
+    public Cargos Cargo { 
+        get => cargo;
+        }
+
+    public int Jubi(){
         if(Edad == 65){
             return 0;
-        } else if (Edad < 65) {
+        }
+        if (Edad < 65) {
             return 65 - Edad;
         }
+        return 0;
     }
 
+    public Empleado(string nom, string ape, DateTime fechaNa, char estadoC, DateTime fechaIn, double sueldoB, Cargos Cargo){
+        nombre = nom;
+        apellido = ape;
+        fechaN = fechaNa;
+        estadoCivil = estadoC;
+        fechaI = fechaIn;
+        sueldoBase = sueldoB;
+        cargo = Cargo;
+    }
+    
+    public double Salario()
+    {
+        double sueldo, adicional;
+
+        if (Antiguo() <= 20)
+        {
+            adicional = SueldoBase * ((double)Antiguo()/100);
+        } else {
+            adicional = SueldoBase * 0.25;
+        }
+
+        if (Cargo == Cargos.ingeniero || Cargo == Cargos.especialista)
+        {
+            adicional += SueldoBase * 0.5;
+        }
+
+        if (EstadoCivil == 'c')
+        {
+            adicional += 150000;
+        }
+
+        sueldo = SueldoBase + adicional;
+        return sueldo;
+    }
+
+    
     
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using InfoEmpleados;
+
+Empleado []preCarga = [
+    new("Matias", "Nieto", new DateTime(1994,02,13), 's', new DateTime(2000, 08, 02), 5000, Cargos.investigador),
+    new("Belen", "German", new DateTime(2004,07,03), 's', new DateTime(2015, 09, 01), 3000, Cargos.auxiliar),
+    new("Emilia", "Gomez", new DateTime(1961,05,15), 'c', new DateTime(1994, 12, 24), 1000, Cargos.especialista)
+];
+
+double montoTotal = 0;
+int prox = 0, cont = 0, indice = 0;
+
+foreach (Empleado item in preCarga)
+{
+    Console.WriteLine(item.Edad);
+    Console.WriteLine(item.Antiguo());
+    Console.WriteLine(item.Jubi());
+    Console.WriteLine($"Sueldo: {item.Salario()}");
+    montoTotal += item.Salario();
+    if (prox == 0)
+    {
+        prox = item.Jubi();
+        indice = cont;
+    }
+
+    if (prox > item.Jubi())
+    {
+        prox = item.Jubi();
+        indice = cont;
+    }
+    cont++;
+}
+
+Console.WriteLine($"Monto Total: {montoTotal}");
+
+Console.WriteLine($"Proximo a jubilarse: {preCarga[indice].Nombre}");
+Console.WriteLine(preCarga[indice].Apellido);
+Console.WriteLine(preCarga[indice].FechaN);
+Console.WriteLine(preCarga[indice].EstadoCivil);
+Console.WriteLine(preCarga[indice].FechaI);
+Console.WriteLine(preCarga[indice].SueldoBase);
+Console.WriteLine(preCarga[indice].Salario());
+Console.WriteLine(preCarga[indice].Cargo);
+Console.WriteLine(preCarga[indice].Edad);
+Console.WriteLine(preCarga[indice].Antiguo());
+Console.WriteLine(preCarga[indice].Jubi());
